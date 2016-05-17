eca79825c7ad5f80d13cba789704edd8e70c3515
Hemanth.HM <hemanth.hm@gmail.com>
2016-05-17 09:22:51 -0700

    xo fix and node >=4

--- test.js
+++ test.js
@@ -3,5 +3,7 @@ import fn from './';
 
 test('image caption', t => {
 	return fn('http://imgur.com/B7a15F5.jpg')
-	.then(res => {t.is(res, 'I think it\'s a dog in the snow.')});
+	.then(res => {
+		t.is(res, 'I think it\'s a dog in the snow.');
+	});
 });
